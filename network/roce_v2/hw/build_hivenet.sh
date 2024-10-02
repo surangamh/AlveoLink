12df4ba0ceb7c3fc982adcc790d95ba01967f21b
Suranga Handagala <s.handagala@northeastern.edu>
2024-10-02 22:53:52 +0000

    updates

--- network/roce_v2/hw/build_hivenet.sh
+++ network/roce_v2/hw/build_hivenet.sh
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [ $1 == 'def' ]
+#!/bin/bash
+
+if [ "$1" == 'def' ]
 then
     cd HiveNet
     if [ ! -d "./build" ]
